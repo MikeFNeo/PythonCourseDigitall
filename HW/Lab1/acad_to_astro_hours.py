5d2e05ac5fcc13afb4673055c2a52fd55d9159cc
MikeFNeo <mihail.filipov94@gmail.com>
2024-01-24 22:07:04 +0200

    Innitial version check

--- HW/Lab1/acad_to_astro_hours.py
+++ HW/Lab1/acad_to_astro_hours.py
@@ -1,20 +1,11 @@
-# ----------------------------------- Task: ---------------------------------- #
-# Given that:
-# An astronomical hour consists of 60 minutes
-# An academical hour consists of 40 minutes
-# For each 3 academical hours there are 15 minutes brake
-# Write a Python program, that will print out how many astronomical hours a python course should take, if it consists of 64 academical hours, including the respective brakes
-# Try to think of a solution, which will be readable enough and easy to maintain in future
-# Write your code in HW/acad_to_astro_hours.py file
-
-# variables declaration
+# Input variables
 minutes_in_astro_hour = 60
 minutes_in_acad_hour = 40
 course_total_acad_hours = 64
 acad_hours_in_a_day = 3
 minutes_in_break = 15
 
-# program logic:
+# execution logic:
 course_days = course_total_acad_hours/acad_hours_in_a_day
 total_breaks_minutes = course_days * minutes_in_break
 
